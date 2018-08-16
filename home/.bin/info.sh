66510321b6b4bc7ad650bdfa6070f8ebe7f6d49c
ChefIronBelly <werthmation@gmail.com>
2018-08-16 10:18:10 -0400

    adds and tweaks

--- home/.bin/info.sh
+++ home/.bin/info.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 # define colors for color-echo
 red="\e[31m"
@@ -16,11 +16,11 @@ f2="\e[0;37m"
 # or use wm array -- add any that need to be recognized
 wms=( 2bwm 2wm 9wm aewm afterstep ahwm alopex amiwm antiwm awesome blackbox bspwm catwm clfswm ctwm cwm dminiwm dragonflywm dwm echinus \
     euclid-wm evilpoison evilwm fluxbox flwm fvwm-crystal goomwwm hcwm herbstluftwm i3 icewm jwm karmen larswm lwm matwm2 mcwm monsterwm \
-    musca notion nwm olwm openbox openbsd-cwm oroborus pekwm ratpoison sapphire sawfish sscrotwm sithwm smallwm snapwm spectrwm stumpwm subtle swm tfwm tinywm tritium twm \
-    uwm vtwm w9wm weewm wind windowlab wm2 wmaker wmfs wmii wmutils wmx xfwm4 xmonad xoat yeahwm )
+    musca notion nwm olwm openbox oroborus pekwm ratpoison sapphire sawfish sscrotwm sithwm smallwm snapwm spectrwm stumpwm subtle swm tfwm tinywm tritium twm \
+    uwm vtwm w9wm weewm wind windowlab wm2 wmaker wmfs wmii wmx xfwm4 xmonad xoat yeahwm )
 
 color-echo() {  # print with colors
-	echo -e "$blu$1: $rst$2"
+	echo -e "$blu$1: $wht$2"
 }
 
 print-kernel() {
@@ -28,7 +28,9 @@ print-kernel() {
 }
 
 print-shell() {
-    color-echo 'SHELL' '    '"$SHELL"
+	#fshell=$(echo $SHELL | cut -d '/' -f 5)
+	fshell=$(echo $SHELL)
+	color-echo 'SHELL' '    '"$fshell"
 }
 
 print-term() {
@@ -66,21 +68,22 @@ print-wm() {
 	        break
         fi
     done
-    #color-echo 'WM' '       '"Not Found / x11fs active"
+    #color-echo 'WM' '       '"Not Found / wmutils active"
 }
            
 print-font() {
-    fontstr=$(xrdb -query 2>/dev/null | grep '*font:')
+    fontstr=$(xrdb -query 2>/dev/null | grep '*.font:')
     font=$(echo $fontstr | awk -F: '{ print $2 }')
-    [[ $font != "" ]] && color-echo 'FONT' '     '"$font"
+    [[ $font != "" ]] && color-echo 'FONT' '    '"$font"
 }
+
 print-distro() {
-    [[ -e /etc/os-release ]] && source /etc/os-release
-    if [ -n "$PRETTY_NAME" ]; then
+	PRETTY_NAME=$(uname)
+	if [[ -n "$PRETTY_NAME" ]]; then
         color-echo 'OS' '       '"$PRETTY_NAME"
-    else
+	else
         color-echo 'OS' '       '"not found"
-    fi
+	fi
 }
 
 print-colors() {
@@ -92,7 +95,7 @@ do
   printf "\e[$((i+30))m░▒▓█▓▒░"
   i=$(($i+1))
 done
-printf "\e[34m░▒▓█\e[0m▓▒░\n\n"
+printf "\e[34m░▒▓█▓▒░\e[0m\n\n"
 # ascii bits by xero
 }
 
@@ -107,7 +110,7 @@ print-shell
 print-term
 #printf "\n"
 print-font
-colors='traffic by dkeg'
+colors='nord'
 printf "\e[34mCOLORS: \e[37m   $colors$rst\n"
 #printf "\n"
 print-disk
