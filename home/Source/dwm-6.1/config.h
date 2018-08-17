6b9d896cab5f8a9b77a6ced251328c27ea6cc910
ChefIronBelly <werthmation@gmail.com>
2018-08-17 14:23:49 -0400

    adds and tweaks

--- home/Source/dwm-6.1/config.h
+++ home/Source/dwm-6.1/config.h
@@ -88,6 +88,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
